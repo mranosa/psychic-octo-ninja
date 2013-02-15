2baeceee04a126e511987db73970ec3157232ecb
mranosa <mark.ranosa@gmail.com>
2013-02-16 06:29:19 +0800

    Added in initial loading screen

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,11 +4,20 @@ psychicOctoNinjaApp.controller('MainCtrl', [
 	'$scope', 'MapService', '$timeout',
 	function($scope, MapService, $timeout) {
 	
+	$scope.message = "Welcome to Quarterland<br>Relax now your Quarter is loading";
 
 	MapService.disable();
 
 	$timeout(function() {
-          MapService.enable();
-    }, 5000);
+		$scope.message = "Before you dig in, some remarks";
+		$timeout(function() {
+			$scope.message = "‘The world, as we perceive it, is our own invention.‘ Heinz von Foerster";
+			$timeout(function() {
+				MapService.enable();
+				$("#map-ui").addClass("hide");
+				//wishlist! transition from blur or transition to blur effect
+			}, 3000);
+		}, 3000);
+	}, 3000);
 
 }]);
