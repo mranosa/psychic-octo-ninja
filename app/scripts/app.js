ebe5f548df2e816a7e03eb769ee921f573c28148
mranosa <mark.ranosa@gmail.com>
2013-02-15 23:59:51 +0800

    Hello Psychic Octo Ninja!

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var psychicOctoNinjaApp = angular.module('psychicOctoNinjaApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+psychicOctoNinjaApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
