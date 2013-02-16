624666a3e6d3b29281ba8f4fbd03d4c31474172b
mranosa <mark.ranosa@gmail.com>
2013-02-17 03:08:08 +0800

    Removed main js cheat and corrected flow

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,24 +1,43 @@
 'use strict';
 
 psychicOctoNinjaApp.controller('MainCtrl', [ 
-	'$scope', 'MapService', '$timeout', '$location',
-	function($scope, MapService, $timeout, $location) {
+	'$scope', 'MapService', '$timeout', '$location', 'NotificationService',
+	function($scope, MapService, $timeout, $location, NotificationService) {
 	
 	$scope.message = "Welcome to Quarterland<br>Relax now your Quarter is loading";
 
-	MapService.disable();
-
-	//TODO remove cheat after
-	$timeout(function() {
-		$scope.message = "Before you dig in, some remarks";
+	function doLoading(){
 		$timeout(function() {
-			$scope.message = "‘The world, as we perceive it, is our own invention.‘ Heinz von Foerster";
+			$scope.message = "Before you dig in, some remarks";
 			$timeout(function() {
-				MapService.enable();
-			$("#map-ui").addClass("hide");
-				$location.path('/dashboard');
+				$scope.message = "‘The world, as we perceive it, is our own invention.‘ Heinz von Foerster";
+				$timeout(function() {
+					MapService.enable();
+					$("#map-ui").addClass("hide");
+					$location.path('/dashboard');
+				}, 3000);
 			}, 3000);
 		}, 3000);
-	}, 3000);
+	}
+
+	MapService.disable();
+
+	//get current location
+	navigator.geolocation.getCurrentPosition(
+		function(position){
+			if(position.coords.accuracy > 50){
+				NotificationService.warning('Geoposition Inaccurate', 'Cant find your current address :(');
+			}
+
+			MapService.init(position);
+			doLoading();
+		},
+		function(position){
+			NotificationService.warning('Geolocation Unavailable', 'geolocation not supported!');
+			MapService.init(position);
+			doLoading();
+		},
+		{ enableHighAccuracy: true, maximumAge: 0, timeout: 30000 }
+	);
 
 }]);
