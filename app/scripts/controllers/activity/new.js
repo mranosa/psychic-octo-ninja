bc4d9fc13ccd7406454fb2738deb99f8c2dc1729
mranosa <mark.ranosa@gmail.com>
2013-02-18 00:02:33 +0800

    Initial implementation of new activity flow

--- app/scripts/controllers/activity/new.js
+++ app/scripts/controllers/activity/new.js
@@ -4,6 +4,56 @@ psychicOctoNinjaApp.controller('Activity/NewCtrl', [
 	'$scope', 'MapService', 'NotificationService', '$timeout', '$location',
  	function($scope, MapService, NotificationService, $timeout, $location) {
 
+ 	$scope.backText = 'Back';
+ 	$scope.nextText = 'Next&nbsp;<i class="icon-arrow-right icon-white"></i>';
+ 	$scope.currIndex = 0;
+ 	$scope.form = {
+ 		title: [
+ 			'What is happening in Quarterland?',
+ 			'Drag your map to your exact position',
+ 			'How long are you going to do it?',
+ 			'How can you be found?',
+ 			'Got something to say?',
+ 			'Review your announcement'
+ 		]
+ 	}
+
+ 	$scope.doNext = function(){
+ 		$scope.currIndex += 1;
+
+ 		if($scope.currIndex === 5){
+ 			$scope.nextText = 'Publish!';
+ 		}
+
+ 		if($scope.currIndex === 6){
+ 			$("#form-new-title").addClass("animated fadeOut");
+ 			$("#ctrl-new-ui-next").addClass("animated fadeOut");
+ 			$("#ctrl-new-ui-back").addClass("animated fadeOut");
+
+	        $timeout(function() {
+				MapService.addMarker(L.marker(MapService.getCurrLatLng()));
+				$location.path('/dashboard');
+			}, 500);
+ 		}
+ 	}
+
+ 	$scope.doBack = function(){
+ 		if($scope.currIndex < 6){
+ 			$scope.nextText = 'Next&nbsp;<i class="icon-arrow-right icon-white"></i>';
+ 		}
+
+ 		if($scope.currIndex === 0){
+ 			$("#form-new-title").addClass("animated fadeOut");
+ 			$("#ctrl-new-ui-next").addClass("animated fadeOut");
+ 			$("#ctrl-new-ui-back").addClass("animated fadeOut");
+
+ 			$timeout(function() {
+	            $location.path('/dashboard');
+	        }, 500);
+ 		}
+
+ 		$scope.currIndex -= 1;	
+ 	};
 
 // ----------------------- test
 //TODO remove me after testing
