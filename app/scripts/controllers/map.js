e79ee08a88ca4be4801ef95d23e80bce40808cbd
mranosa <mark.ranosa@gmail.com>
2013-02-21 23:51:22 +0800

    It worked! Biatch! Now remove the test

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -3,6 +3,5 @@
 psychicOctoNinjaApp.controller('MapCtrl', [ 
 	'$scope', 'NotificationService', 'MapService', 'CookieIDService',
 	function($scope, NotificationService, MapService, CookieIDService) {
-		console.log(CookieIDService.getId());
-		console.log($.cookie());
+		
 }]);
