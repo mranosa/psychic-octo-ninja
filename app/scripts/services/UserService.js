bb979da05fa87b0c6ece6e1a6e9e114e2d0687a5
mranosa <mark.ranosa@gmail.com>
2013-02-22 23:38:14 +0800

    Initial UserService implementation

---
+++ app/scripts/services/UserService.js
@@ -0,0 +1,15 @@
+'use strict';
+
+psychicOctoNinjaApp.factory('UserService', function() {
+  // Service logic
+  // ...
+
+  var meaningOfLife = 42;
+
+  // Public API here
+  return {
+    someMethod: function() {
+      return meaningOfLife;
+    }
+  };
+});
