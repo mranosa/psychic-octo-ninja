18906de3ca2ccaaab4b018927591a1c6d311e711
mranosa <mark.ranosa@gmail.com>
2013-02-23 00:41:30 +0800

    Initial user service logic implementation

--- app/scripts/services/UserService.js
+++ app/scripts/services/UserService.js
@@ -1,15 +1,30 @@
 'use strict';
 
-psychicOctoNinjaApp.factory('UserService', function() {
-  // Service logic
-  // ...
+psychicOctoNinjaApp.factory('UserService', ['CookieIDService', 'NotificationService', function(CookieIDService, NotificationService) {
 
-  var meaningOfLife = 42;
+	var userRef = new Firebase('https://hanael.firebaseIO.com/users/' + CookieIDService.getId());
+	var userStatus;
+	var userEvent;
 
-  // Public API here
-  return {
-    someMethod: function() {
-      return meaningOfLife;
-    }
-  };
-});
+	var UserService = function() {
+		userRef.once('value', function(dataSnapshot) {
+			if(dataSnapshot.val() === null){
+				userRef.update({status: 'bum', event: null});
+			}
+		});
+		userRef.on('value', function(dataSnapshot) {
+			var currUserInfo = dataSnapshot.val();
+			NotificationService.info('Status Changed', 'Your status is now ' + currUserInfo.status);
+			userStatus = currUserInfo.status;
+			userEvent = currUserInfo.event;
+		});
+	};
+
+	UserService.prototype = {
+		getStatus: function(){
+			return userStatus;
+		}
+	}
+
+	return new UserService();
+}]);
